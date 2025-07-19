ccd3a7e8960b2169df65c740a493dc40d5095637
Pasindu <pasindusathsara60@gmail.com>
2025-07-19 09:15:34 +0530

    Initial commit

---
+++ src/test/java/com/midlane/project_management_tool_api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midlane.project_management_tool_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
